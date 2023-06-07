24adda0a3e9d5f2fbc5a5f2a34121b37a22d7e09
kongmenglor <lorxx504@umn.edu>
2023-06-07 02:20:40 -0500

    Photo is added, able to switch from pictures.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy} from '@angular/core';
 import {AccountService} from "../account.service";
 import {IEmployee} from "../interfaces/IEmployee";
 import {NgbModal, NgbModalConfig} from "@ng-bootstrap/ng-bootstrap";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-main',
@@ -22,10 +23,34 @@ export class MainComponent implements OnDestroy{
         console.log(err);
       }
     });
+    this.currentImage = this.images[this.index];
   }
+
+  onClickRight() {
+    this.index += 1;
+    if (this.index === 8) {
+      this.index = 0;
+    }
+    this.currentImage = this.images[this.index];
+  }
+
+  index: number = 0;
+  currentImage: string = "";
+  images: string[] = [
+    `${environment.imageApiUrl}1.png`,
+    `${environment.imageApiUrl}2.png`,
+    `${environment.imageApiUrl}3.png`,
+    `${environment.imageApiUrl}4.png`,
+    `${environment.imageApiUrl}5.png`,
+    `${environment.imageApiUrl}6.png`,
+    `${environment.imageApiUrl}7.png`,
+    `${environment.imageApiUrl}8.png`
+  ]
   employees: IEmployee[] = [];
 
   public open(content: any) {
+    this.index = 0;
+    this.currentImage = this.images[this.index];
     this.modalService.open(content);
   }
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  imageApiUrl: `https://bootdey.com/img/Content/avatar/avatar`,
   apiBaseUrl: `http://localhost:8008/api/employee/`
 };
